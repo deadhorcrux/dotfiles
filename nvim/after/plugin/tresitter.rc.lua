02f978c9f6ede0423453ae9202bab0a009f6d7fe
deadhorcrux <kovzalov.aleksandr@mail.ru>
2023-12-09 19:35:19 +0400

    ㅤ

---
+++ nvim/after/plugin/tresitter.rc.lua
@@ -0,0 +1,22 @@
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = false,
+    disable = {},
+  },
+  ensure_installed = {
+    "python",
+    "json",
+    "html",
+    "css"
+  },
+  autotag = {
+    enable = true,
+  }
+}
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
---
+++ nvim/lua/maps.lua
@@ -0,0 +1,43 @@
+local keymap = vim.keymap
+
+keymap.set('n', 'x', '"_x')
+
+-- Increment/decrement
+keymap.set('n', '+', '<C-a>')
+keymap.set('n', '-', '<C-x>')
+
+-- Delete a word backwards
+keymap.set('n', 'dw', 'vb"_d')
+
+-- Select all
+keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- Save with root permission (not working for now)
+--vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
+
+-- Tab
+keymap.set('n', 'te', ':tabedit<Return>')
+keymap.set('n', 'tp', ':tabprev<Return>')
+keymap.set('n', 'tn', ':tabnext<Return>')
+
+-- Search 
+keymap.set('n', '<Space>', '/')
+
+-- Split window
+keymap.set('n', 'ss', ':split<Return><C-w>w')
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
+-- Move window
+keymap.set('', 'sh', '<C-w>h')
+keymap.set('', 'sk', '<C-w>k')
+keymap.set('', 'sj', '<C-w>j')
+keymap.set('', 'sl', '<C-w>l')
+
+-- Ex mode
+keymap.set('n', 'si', ':Ex<Return>')
+
+-- Resize window
+keymap.set('n', '<C-w><left>', '<C-w><')
+keymap.set('n', '<C-w><right>', '<C-w>>')
+keymap.set('n', '<C-w><up>', '<C-w>+')
+keymap.set('n', '<C-w><down>', '<C-w>-')
